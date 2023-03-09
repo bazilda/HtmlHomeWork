203ccb463fa4d618b41c21507a1e5b8e49a6877f
bazilda <bazilda@gmail.com>
2023-03-09 19:22:55 +0200

    HW-31 fixed version

--- react-application/src/components/Timer.tsx
+++ react-application/src/components/Timer.tsx
@@ -1,4 +1,6 @@
+
 import React from "react"
+import { setInterval } from "timers/promises";
 export const Timer: React.FC = () => {
     const styles: React.CSSProperties = { backgroundColor: "lightblue", fontSize: "2em" }
     let col1 = "red";
@@ -7,21 +9,20 @@ export const Timer: React.FC = () => {
     setTimeout(repaint, 10000);
     const [time, setTime] = React.useState(new Date());
     const [col, newCol] = React.useState(col1);
-
     function tic() {
-
         setTime(new Date());
     }
     function switchColor() {
-        [col1, col2] = [col2, col1];
+        if (col == col1) {
+            col1 = col2;
+        }
         return col1;
     }
     function repaint() {
         newCol(switchColor());
     }
-
     return <div>
-        <h2 style={ {color: col}}>Current Time</h2>
+        <h2 style={{ color: col }}>Current Time</h2>
         <p style={styles}>{time.toLocaleTimeString()}</p>
     </div>
 }
